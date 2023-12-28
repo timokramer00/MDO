726ed249e3d98670102b9eae9fde063738067c6e
timokramer00 <timokramer01@gmail.com>
2023-12-28 13:10:31 +0100

    changes to Q3D function

--- Q3D_Loads_func.m
+++ Q3D_Loads_func.m
@@ -1,9 +1,10 @@
 %% Aerodynamic solver setting
 function [out]=Q3D_Loads_func(b,cr,TRi,TRo,berncoef,hcruise,Wstr,Wfuel)
 
-LEsweep=atan((cr-cr*TRi)/6.048);
+WAW=90000; %kg
 
 % Wing planform geometry 
+LEsweep=atan((cr-cr*TRi)/6.048);
 %                x    y     z   chord(m)    twist angle (deg) 
 AC.Wing.Geom = [0     0     0     cr         0;
                 6.048*tan(LEsweep)  6.048   0     cr*TRi+0.001         0;
@@ -24,14 +25,16 @@ AC.Wing.eta = [0;1];  % Spanwise location of the airfoil sections
 AC.Visc  = 1;              % 0 for inviscid and 1 for viscous analysis
 
 % Flight Condition
-gamma=1.4;
 ISA = atmosisa(hcruise);
+nmax=2.5;
+A=((AC.Wing.Geom(1,4)+AC.Wing.Geom(2,4))*AC.Wing.Geom(2,2))/2+((AC.Wing.Geom(2,4)+AC.Wing.Geom(3,4))*AC.Wing.Geom(3,2))/2;
 AC.Aero.V     = 250.0200;            % flight speed (m/s)
 AC.Aero.rho   = 1.225;         % air density  (kg/m3)
 AC.Aero.alt   = hcruise;             % flight altitude (m)
 AC.Aero.Re    = 1.14e7;        % reynolds number (bqased on mean aerodynamic chord)
-AC.Aero.M     = AC.Aero.V/ISA(2);           % flight Mach number 
-AC.Aero.CL    = 2.4;          % lift coefficient - comment this line to run the code for given alpha%
+AC.Aero.M     = AC.Aero.V/ISA(2);           % flight Mach number
+L=(Wstr+Wfuel+WAW)*nmax;
+AC.Aero.CL    = L/(0.5*AC.Aero.rho*A*AC.Aero.V^2);          % lift coefficient - comment this line to run the code for given alpha%
 %AC.Aero.Alpha = 2;             % angle of attack -  comment this line to run the code for given cl 
 
 
