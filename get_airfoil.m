1ceed43f8b7f4d352d16b1641f00d30381f85cc1
timokramer00 <timokramer01@gmail.com>
2024-01-03 17:21:41 +0100

    Added airfoil function

---
+++ get_airfoil.m
@@ -0,0 +1,18 @@
+function get_airfoil(Au,Al)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+X_vect = linspace(0,1,99)'; 
+[Xtu,Xtl,C,Thu,Thl,Cm] = D_airfoil2(Au,Al,X_vect);
+Xtu = flipud(Xtu);
+Xtu(length(Xtu),:)=[];
+
+
+X=[Xtu;Xtl];
+
+
+fid = fopen('airfoil.dat', 'wt');
+for j = 1:length(X(:,1))
+    fprintf(fid, '%f %f\n', X(j,1),X(j,2));
+end
+
+fclose(fid);
